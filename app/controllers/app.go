d6e42a6d565a1c0316e765b15699c54f8bae3b71
SarahhhC <joytutu29@naver.com>
2015-11-25 21:28:55 +0900

    test css

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -9,3 +9,25 @@ type App struct {
 func (c App) Index() revel.Result {
 	return c.Render()
 }
+
+func (c App) Me() revel.Result {
+	name := "csr"
+	return c.Render(name)
+}
+
+func (c App) MeNumber(id int) revel.Result {
+	return c.Render(id)
+}
+
+func (c App) MePost() revel.Result {
+	return c.Render()
+}
+
+func (c App) MePostSet() revel.Result {
+	password := c.Params.Get("password")
+	if password == "1234" {
+		return c.Redirect(App.Index)
+	} else {
+		return c.Redirect(App.MePost)
+	}
+}
